eedb22ac0c9038eb2a10d10a7f0cae04d9c9e429
btee <btee@protonmail.com>
2020-12-28 12:11:00 +0100

    Trim strings when using Newtonsoft.Json.JsonConvert

---
+++ AltradyNotifier/Logic/Converters.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace AltradyNotifier.Logic
+{
+    public static class Converters
+    {
+        public class TrimmingConverter : JsonConverter
+        {
+            public override bool CanRead => true;
+
+            public override bool CanWrite => true;
+
+            public override bool CanConvert(Type objectType) => objectType == typeof(string);
+
+            public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+            {
+                return (reader.Value as string ?? string.Empty).Trim();
+            }
+
+            public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+            {
+                writer.WriteValue((value as string ?? string.Empty).Trim());
+            }
+        }
+    }
+}
